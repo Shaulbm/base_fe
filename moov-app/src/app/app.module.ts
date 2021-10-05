d473f5c98965834e67d67d226caabf0c56f7cb20
hadarbmdev <hadarbmdev@gmail.com>
2021-10-05 13:08:27 +0300

    Add feature modules to root module import

--- moov-app/src/app/app.module.ts
+++ moov-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiscoveryProcessModule } from './discovery-process/discovery-process.module';
+import { PersonalViewModule } from './personal-view/personal-view.module';
+import { TeamViewModule } from './team-view/team-view.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DiscoveryProcessModule,
+    PersonalViewModule,
+    TeamViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
